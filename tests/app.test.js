2cd018d3dc28eb5685a00a76fb19afb385e49e4d
kambelkouame <oikambelkouame@gmail.com>
2025-10-04 13:03:46 +0000

    hello docker +ci

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,20 @@
 const request = require('supertest');
 const app = require('../index');
 
-describe('GET /', () => {
+let server;
+
+beforeAll(() => {
+  server = app.listen(3000); // démarre le serveur pour les tests
+});
+
+afterAll((done) => {
+  server.close(done); // ferme le serveur après les tests
+});
+
+describe('Test endpoint /', () => {
   it('should return 200 and the hello message', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
+    const res = await request(server).get('/');
+    expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello Docker + CI/CD!');
   });
 });
